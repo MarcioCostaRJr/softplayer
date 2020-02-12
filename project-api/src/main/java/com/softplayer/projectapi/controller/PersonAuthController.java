aaa28219294d4a3c8ac02acd8b9e39b9670d277b
Marcio Costa <marcio_crj@hotmail.com>
2020-02-12 11:28:10 -0300

    Add Router in Controller for Authentication

---
+++ project-api/src/main/java/com/softplayer/projectapi/controller/PersonAuthController.java
@@ -0,0 +1,26 @@
+/**
+ *
+ */
+package com.softplayer.projectapi.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.softplayer.projectapi.model.Person;
+
+/**
+ * @author local-personal
+ *
+ */
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping({"/*"})
+public class PersonAuthController {
+
+    @GetMapping("/")
+    public Person authentication() {
+        return new Person();
+    }
+}
